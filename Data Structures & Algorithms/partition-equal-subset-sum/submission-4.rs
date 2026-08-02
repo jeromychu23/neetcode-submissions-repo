193e76f3687f6bbff7089cea44442b6b29f5e82a
yu1005 <109812036+jeromychu23@users.noreply.github.com>
2026-08-02 17:27:12 +0800

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn can_partition(nums: Vec<i32>) -> bool {
+        let total: usize = nums.iter().map(|&num| num as usize).sum();
+        
+        if total % 2 != 0 {
+            return false;
+        }
+
+        let target = total / 2;
+        let mut dp = vec![false; target + 1];
+        dp[0] = true;
+
+        for num in nums {
+            let value = num as usize;
+            
+            for cur_sum in (value..=target).rev() {
+                dp[cur_sum] = dp[cur_sum] || dp[cur_sum - value]
+            }
+        }
+
+        dp[target]
+
+    }
+}
