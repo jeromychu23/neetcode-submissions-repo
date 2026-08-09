7415044023aae3ec019bebfb644fe39d91009247
yu1005 <109812036+jeromychu23@users.noreply.github.com>
2026-08-09 23:01:38 +0800

    Add: reverse-bits - submission-4

---
+++ Data Structures & Algorithms/reverse-bits/submission-4.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn reverse_bits(mut n: u32) -> u32 {
+        let mut res = 0;
+        for _ in 0..32 {
+            let bit = n & 1;
+            res = (res << 1) + bit;
+            n >>= 1
+        }
+        res
+    }
+}
