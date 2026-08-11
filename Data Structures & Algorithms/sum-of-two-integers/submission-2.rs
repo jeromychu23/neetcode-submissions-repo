401658824d7136e2905545a5a8a531c6a90beb98
yu1005 <109812036+jeromychu23@users.noreply.github.com>
2026-08-11 22:33:08 +0800

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn get_sum(a: i32, b: i32) -> i32 {
+        let mut a = a;
+        let mut b = b;
+
+        while b != 0 {
+            let tmp = (a & b) << 1;
+            a ^= b;
+            b = tmp
+        }
+        a
+    }
+}
